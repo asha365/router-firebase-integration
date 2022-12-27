8bf324528bb3ac27114f2620c07ea65b2b6c9183
Asha <asha.engrbd@gmail.com>
2022-12-27 11:49:51 +0600

    added onauthstate change to parsis the auth

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import useFirebase from '../../hooks/useFirebase';
 
 const Login = () => {
+    const {signInWithGoogle} = useFirebase();
     return (
         <div>
             <h1>Please login</h1>
+            <div style={{margin: '20px'}}>
+                <button onClick={signInWithGoogle}>Google Sign In</button>
+            </div>
+            <form>
+                <input type="email" name="" placeholder='Your Email' id="" />
+                <br/>
+                <input type="password" name="" placeholder='Password' id="" />
+                <br/>
+                <input type="submit" value="Login"  />
+            </form>
         </div>
     );
 };
---
+++ src/components/Register/Register.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Register = () => {
+    return (
+        <div>
+            <h3>Please Register Now</h3>
+            <form>
+                <input type="text" name="" placeholder='Your Name' id="" />
+                <br/>
+                <input type="email" name="" placeholder='Your Email' id="" />
+                <br/>
+                <input type="password" name="" placeholder='Password' id="" />
+                <br/>
+                <input type="submit" value="Register"  />
+            </form>
+        </div>
+    );
+};
+
+export default Register;
